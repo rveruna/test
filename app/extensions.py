02639eb2765fb1b985a423368591b571f0048aaa
Veronika <40212551@live.napier.ac.uk>
2016-11-22 17:32:42 +0000

    6

--- app/extensions.py
+++ app/extensions.py
@@ -3,9 +3,9 @@ from flask.ext.sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 # Flask-Login
-from flask.ext.login import LoginManager
+from flask_login import LoginManager
 login_manager = LoginManager()
 
 # Flask-WTF csrf protection
 from flask_wtf.csrf import CsrfProtect
-csrf = CsrfProtect()  
+csrf = CsrfProtect()
