8cf12aeffa18d8e730cd109604ac6e4a2a8e2464
Veronika <40212551@live.napier.ac.uk>
2016-11-22 18:33:01 +0000

    1

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return "Hello Boss!  <a href='/logout'>Logout</a>"
+        return "Hello Boss!"
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
@@ -24,7 +24,6 @@ def logout():
     session['logged_in'] = False
     return home()
 
-
-if __name__ == "__main__":
+ if __name__ == "__main__":
     app.secret_key = os.urandom(12)
     app.run(debug=True,host='0.0.0.0', port=5000)
